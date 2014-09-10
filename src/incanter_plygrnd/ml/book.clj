b502c94fb45d678fe9df96ee67c440c34288c3c9
Halit Olali <halitolali@gmail.com>
2014-09-10 16:42:43 +0300

    lr

--- src/incanter_plygrnd/ml/book.clj
+++ src/incanter_plygrnd/ml/book.clj
@@ -53,16 +53,17 @@ A
 
 ;linear regression
 (def X (cl/matrix [8.401 14.475 13.396 12.127 5.044
-                   8.339 15.692 17.108 9.253 12.029]))
+                   8.339 15.692 17.108 9.253 12.029
+                   22.112]))
 X
 (def Y (cl/matrix [-1.57 2.32 0.424 0.814 -2.3
-                   0.01 1.954 2.296 -0.635 0.328]))
+                   0.01 1.954 2.296 -0.635 0.328 5.444]))
 Y
 
 (def linear-samp-scatter
   (charts/scatter-plot X Y))
 
-(defn plot-scatter
+(defn plot-scatter []
   (inct/view linear-samp-scatter))
 
 (plot-scatter)
